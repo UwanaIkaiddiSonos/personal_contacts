b90bb756bc4679e28cbaf36d3b5dddada7553559
Uwana <uwikaiddi@gmail.com>
2019-03-04 19:37:24 -0600

    Add get and create

--- app/controllers/api/personal_contacts_controller.rb
+++ app/controllers/api/personal_contacts_controller.rb
@@ -1,2 +1,13 @@
 class Api::PersonalContactsController < ApplicationController
+  
+  def get
+    render 'get.json.jbuilder'
+  end
+
+  def create
+    render 'create.json.jbuilder'
+  end
+
+  def 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get '/get_url' => 'personal_contacts#get'
+    post 'post_url' => 'personal_contacts#create'
+    patch 'patch_url' => 'personal_contacts#update'
+  end
 end
