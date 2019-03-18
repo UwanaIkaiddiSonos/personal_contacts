c2904505c96a7b932b611356f4dd9120318b0627
Uwana <uwikaiddi@gmail.com>
2019-03-18 07:21:44 -0500

    Add orders controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,2 +1,20 @@
 class Api::OrdersController < ApplicationController
+  def index
+    if current_user
+      @orders = current_user.orders
+      render 'index.json.jbuilder'
+    else
+      render json: [], status: :unauthorized
+    end
+  end
+
+  def create
+    @order = Order.new(
+      #user: :current_user,
+      first_name: params[:first_name],
+      deploy_status: params[:deploy_status]
+    )
+    order.save
+
+  end
 end
